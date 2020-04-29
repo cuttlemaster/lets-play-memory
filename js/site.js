eb9c044fd7a7f6a0fd4dec7ae8aac1c75661f54e
Caleb Flanagan <flanagan.caleb@gmail.com>
2020-04-28 21:17:15 -0600

    basic show/hide of elements added
    Signed-off-by: Caleb Flanagan <flanagan.caleb@gmail.com>

--- js/site.js
+++ js/site.js
@@ -1,3 +1,55 @@
 document.addEventListener("DOMContentLoaded", () => {
-  console.log('the page is ready now');
+
+  // store various elements into arrays and consts for use
+  const boardSize = [...document.querySelectorAll('header button')];
+  const imageType = [...document.querySelectorAll('.image-selection section button')];
+
+  const imageSelection = document.querySelector('.image-selection');
+  const startGame = document.querySelector('.start-game');
+  const gameBoard = document.querySelector('.game-board');
+
+  // whenever a game size button is clicked, remove the active state
+  // from all buttons and then add the active state back to the once clicked
+  boardSize.forEach( (button) => {
+    button.addEventListener('click', (e) => {
+      boardSize.forEach( (activeButton) => {
+        activeButton.classList.remove('active');
+      });
+
+      if (button.classList.contains('active')) {
+        button.classList.remove('active');
+      } else {
+        button.classList.add('active');
+      }
+    });
+  });
+
+  // whenever an image type button is clicked, remove the active state
+  // from all buttons and then add the active state back to the once clicked
+  imageType.forEach( (button) => {
+    button.addEventListener('click', (e) => {
+      imageType.forEach( (activeButton) => {
+        activeButton.classList.remove('active');
+      });
+
+      if (button.classList.contains('active')) {
+        button.classList.remove('active');
+      } else {
+        button.classList.add('active');
+      }
+
+      // show the start game button after image type selection
+      startGame.classList.remove('hide');
+    });
+  });
+
+
+  // when the "start game" text is clicked, hide the image type selection
+  // and reveal the game board tiles to the user so the game can begin
+  startGame.addEventListener('click', (e) => {
+    e.target.classList.add('hide');
+    imageSelection.classList.add('hide');
+    gameBoard.classList.remove('hide');
+  });
+
 });
